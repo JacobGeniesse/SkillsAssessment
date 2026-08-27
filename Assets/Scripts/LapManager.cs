0a9666045b3b787adb973880aba019045680511c
Jacob Geniesse <jacobgeniesse@gmail.com>
2026-08-27 14:11:15 -0400

    InProgressLapTracker
    LapTracker Progress in class.

--- Assets/Scripts/LapManager.cs
+++ Assets/Scripts/LapManager.cs
@@ -1,11 +1,15 @@
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class LapManager : MonoBehaviour
 {
+    private List<GameObject> Runners = new List<GameObject>();
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        //Runners = FindGameObjectsWithTag("Runner").ToList();
     }
 
     // Update is called once per frame
